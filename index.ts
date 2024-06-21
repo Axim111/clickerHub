ed4a9a95e389521f2153e26f0616a18c36424a4a
Axim <makseropolov@gmail.com>
2024-06-21 17:19:51 +0500

    fix port again

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, async () => {
+app.listen(8443, async () => {
   // console.log("Server running on port 3000");
   await setWebhook()
 });
