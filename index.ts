d1e81f8f6186ca0c8565a65822bd35d386ca0355
Axim <makseropolov@gmail.com>
2024-06-21 21:44:48 +0500

    again

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Welcome111'))
 const WEBHOOK_URL = `${process.env.BOT_WEBHOOK}`;
 
 const TELEGRAM_API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
@@ -28,7 +28,17 @@ const setWebhook = async () => {
 };
 
 
-bot.launch(),
+bot.launch({
+  webhook: {
+
+    domain: `${process.env.BOT_WEBHOOK}`,
+
+
+    port: 8443
+
+
+  },
+});
 
 
 
@@ -36,7 +46,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(8443, async () => {
+app.listen(3000, async () => {
   // console.log("Server running on port 3000");
   await setWebhook()
 });
