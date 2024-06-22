6415bf3ac76942c33730ef2aa0e7b6a371946cb7
Axim <makseropolov@gmail.com>
2024-06-22 08:43:44 +0500

    -0.5 webhook again

--- index.ts
+++ index.ts
@@ -16,16 +16,7 @@ bot.start((ctx) => ctx.reply('Welcome111'))
 const WEBHOOK_URL = `${process.env.BOT_WEBHOOK}`;
 
 const TELEGRAM_API = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
-const setWebhook = async () => {
-  try {
-    const response = await axios.post(
-      `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+
 
 
 bot.launch({
@@ -48,5 +39,5 @@ app.get("/", (req, res) => {
 
 app.listen(3000, async () => {
   // console.log("Server running on port 3000");
-  await setWebhook()
+
 });
