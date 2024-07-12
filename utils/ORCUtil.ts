67d38551998c7376aab781f2b0d0d1aa8ba03946
Axim <makseropolov@gmail.com>
2024-07-12 21:28:13 +0500

    work now

--- utils/ORCUtil.ts
+++ utils/ORCUtil.ts
@@ -1,9 +1,8 @@
-import { createWorker } from 'tesseract.js';
+import { createWorker } from 'tesseract.js'
 
-export const ORCUtil = async (path:string, len:string) => {
-  const worker = await createWorker(len);
-  const ret = await worker.recognize(path);
-  await worker.terminate();
+export const ORCUtil = async (path: string, len: string) => {
+  const worker = await createWorker(len)
+  const ret = await worker.recognize(path)
+  await worker.terminate()
   return ret.data.text
-  
-};
+}
